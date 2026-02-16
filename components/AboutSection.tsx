c0541b44b1fe3207e3f9f5e7030744e7da5d7bf0
Abhay Tiwari <abhay.tiwari3003@gmail.com>
2026-02-17 01:03:56 +0530

    change3

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -20,7 +20,7 @@ const textReveal: Variants = {
   visible: { 
     y: "0%", 
     opacity: 1, 
-    transition: { duration: 0.8, ease: [0.22, 1, 0.36, 1] } 
+    transition: { duration: 0.8, ease: [0.22, 1, 0.36, 1] as const } 
   }
 };
 
